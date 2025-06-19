9db58769d302b60b7e42f14fc56f9fa74c7e15cd
Nate Schultz <nate.schultz@outlook.com>
2025-06-19 16:47:52 -0400

    misc

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import requests
 import os
 
+COORDINATE_LATITUDE = 43.528484
+COORDINATE_LONGITUDE = -116.147614
+RADIUS = 2.03  # in miles
+
 
 def call_us_census_api(endpoint: str, params: dict, api_key: str = None):
     """
@@ -26,11 +30,41 @@ def call_us_census_api(endpoint: str, params: dict, api_key: str = None):
 
 def run():
     api_key = os.getenv('CENSUS_API_KEY')
-    print(call_us_census_api(
+
+    # Example call to get state names
+    # print(call_us_census_api(
+    #     'https://api.census.gov/data/2020/dec/pl',
+    #     {'get': 'NAME', 'for': 'state:*'},
+    #     api_key
+    # ))
+
+    # Get census block FIPS code for the given lat/lon
+    fcc_url = 'https://geo.fcc.gov/api/census/block/find'
+    fcc_params = {
+        'latitude': COORDINATE_LATITUDE,
+        'longitude': COORDINATE_LONGITUDE,
+        'format': 'json'
+    }
+    fcc_response = requests.get(fcc_url, params=fcc_params)
+    fcc_response.raise_for_status()
+    fcc_data = fcc_response.json()
+    block_fips = fcc_data['Block']['FIPS']
+    state_fips = block_fips[:2]
+    county_fips = block_fips[2:5]
+    tract_code = block_fips[5:11]
+    block_code = block_fips[11:]
+
+    # Get population for the census block
+    population_response = call_us_census_api(
         'https://api.census.gov/data/2020/dec/pl',
-        {'get': 'NAME', 'for': 'state:*'},
+        {
+            'get': 'P1_001N',  # Total population
+            'for': f'block:{block_code}',
+            'in': f'state:{state_fips} county:{county_fips} tract:{tract_code}'
+        },
         api_key
-    ))
+    )
+    print('Population for census block:', population_response)
 
 
 if __name__ == "__main__":
